db0a585312b73310bb1c86179f9d6a0a0e0f54ae
Emily Soth <esoth@ucsc.edu>
2020-04-02 18:35:11 -0700

    first working draft

---
+++ no-covid.js
@@ -0,0 +1,46 @@
+
+// Filters out elements that include COVID-19 related keywords
+
+let stack = [];
+var order = [];
+var content;
+var count = 0;
+
+// visit all nodes in order of DFS
+// record the order they are visited in the order list
+
+stack.push(document.body);
+while (stack.length !== 0) {
+    let current = stack.pop();
+
+    order.push(current);
+
+    if (current && current.hasChildNodes()) {
+        var i;
+        for (i = 0; i < current.childNodes.length; i++) {
+            stack.push(current.childNodes[i]);
+        }
+    }
+}
+
+// go through the nodes in reverse DFS order
+// this way, for any node, all its children are filtered before it is
+// so we remove the minimum amount of content
+for (i = order.length - 1; i >= 0; i--) {
+
+    content = order[i].innerHTML
+
+    if (content) {
+        // make lower case so content can be compared easily
+        content = content.toLowerCase();
+
+        // check for covid-19 keywords and remove element content if found
+        if (content.includes('covid') || content.includes('coronavirus')) {
+            count++;  // keep count of how many elements are filtered
+            order[i].innerHTML = "";
+        }
+    }
+}
+
+// log how many elements were filtered
+console.log(count.toString() + " COVID-related elements filtered out.")
