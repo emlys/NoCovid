ef06d5b6b849f9e491d3ec0a9308d2292654a364
Emily Soth <esoth@ucsc.edu>
2020-04-09 17:57:31 -0700

    refactored to do filtering in one pass

--- no-covid.js
+++ no-covid.js
@@ -1,33 +1,8 @@
 // Filters out elements that include COVID-19 related keywords
 
-var stack = [];
-var order = [];
-var content;
-var current;
-var count = 0;
-
-// visit all nodes in order of DFS
-// record the order they are visited in the order list
-console.log("here");
-stack.push(document.body);
-while (stack.length !== 0) {
-    current = stack.pop();
-
-    order.push(current);
-
-    if (current && current.hasChildNodes()) {
-        for (let i = 0; i < current.childNodes.length; i++) {
-            stack.push(current.childNodes[i]);
-        }
-    }
-}
-
-// go through the nodes in reverse DFS order
-// this way, for any node, all its children are filtered before it is
-// so we remove the minimum amount of content
-for (i = order.length - 1; i >= 0; i--) {
-
-    content = order[i].innerHTML
+function filter(node) {
+    var content = node.innerHTML;
+    var count = 0;
 
     if (content) {
         // make lower case so content can be compared easily
@@ -36,10 +11,28 @@ for (i = order.length - 1; i >= 0; i--) {
         // check for covid-19 keywords and remove element content if found
         if (content.includes('covid') || content.includes('coronavirus')) {
             count++;  // keep count of how many elements are filtered
-            order[i].innerHTML = "";
+            node.innerHTML = "";
         }
     }
+    return count;
 }
 
+// go through the nodes in reverse DFS order
+// this way, for any node, all its children are filtered before it is
+// so we remove the minimum amount of content
+function filterAll(node) {
+    var count = 0;
+    if (node && node.hasChildNodes()) {
+        for (let i = 0; i < node.childNodes.length; i++) {
+            count = filterAll(node.childNodes[i]);
+        }
+    }
+    count += filter(node, count);
+    return count;
+}
+
+
+var total = filterAll(document.body);
+
 // log how many elements were filtered
-console.log(count.toString() + " COVID-related elements filtered out.")
+console.log(total.toString() + " COVID-related elements filtered out.")
